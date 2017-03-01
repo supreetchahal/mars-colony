43bd637d9ed97c73bec93074037e9b6114a32fc6
Supreet Chahal <supreet_chahal@hotmail.com>
2017-02-28 20:22:18 -0800

    Edited home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  Mars Colony
-  <a routerLink="/register">Register page </a>
-  <a routerLink="/encounters">Encounters page </a>
-  <a routerLink="/report">Report page </a>
-  <a routerLink="/notfound">Not Found page </a>
   <router-outlet></router-outlet>
   `,
   styleUrls: ['./app.component.css']
